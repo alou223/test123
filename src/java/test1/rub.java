0129ff072ff7d33abdc748ecbb98ba0acf7511e1
alou223 <gavrushowai@gmail.com>
2022-08-20 11:49:22 +0300

    123

--- src/java/test1/rub.java
+++ src/java/test1/rub.java
@@ -5,11 +5,22 @@ public class rub {
     public static void main(String[] args){
         final double ROUBLES_PER_DOLLAR = 72.12;
         int dollars;
-        double roubles;
+        double roubles = 0;
+        double digit = 0;
+
         Scanner input=new Scanner(System.in);
+
         System.out.println("Введите сумму в долларах: ");
         dollars=input.nextInt();
         roubles=ROUBLES_PER_DOLLAR * dollars;
-        System.out.println("Сумма в рублях равна: " + roubles);
+        digit = dollars%10;
+
+        if(dollars>=5 && dollars<=20)
+            System.out.println(+dollars + " Американских долларов равны " +roubles);
+        if(digit==1)
+            System.out.println(+dollars + " Американский доллар равен " +roubles);
+        else if(digit >=2 && digit <=4)
+            System.out.println(+dollars + " Американских доллара равны " +roubles);
+
     }
 }
